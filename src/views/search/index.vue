6ecdc040f59131d8562c680c9995e4c6b513832e
Your Name <you@example.com>
2019-10-16 10:29:05 +0800

    本地存储

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -4,11 +4,20 @@
     <div class="nav">
       <van-nav-bar title="猫眼电影" left-arrow />
     </div>
+    <div class="search-wrapper">
+      <div class="search-header">
+        <form action="/">
+          <van-search placeholder="搜电影、搜影院" show-action />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .page-search {
+  display: flex;
+  flex-direction: column;
   .van-nav-bar {
     height: 52px;
     background: #e54847;
@@ -21,6 +30,21 @@
     .van-icon-arrow-left:before {
       color: #fff;
       font-size: 30px;
+    }
+  }
+  .search-wrapper {
+    background: #f5f5f5;
+    height: 100vh;
+    .van-search__action {
+      color: #f03d37;
+      height: 30px;
+      line-height: 30px;
+      padding: 0 10px;
+      font-size: 16px;
+      // border-bottom: 1px solid #f5f5f5;
+    }
+    .van-search {
+      border-bottom: 1px solid #e5e5e5;
     }
   }
 }
